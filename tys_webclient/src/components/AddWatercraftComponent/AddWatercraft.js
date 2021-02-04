cd1f75656cb2d849b36e8c7f4258c755fc2f214b
Saksham Jhawar <sakshamjhawar@Sakshams-MacBook-Pro.local>
2021-02-03 18:42:02 -0700

    Task#31 - Added fields to form

--- tys_webclient/src/components/AddWatercraftComponent/AddWatercraft.js
+++ tys_webclient/src/components/AddWatercraftComponent/AddWatercraft.js
@@ -1,13 +1,20 @@
 import React, {useState} from 'react'
-import { Grid, makeStyles, TextField, Select, InputLabel, MenuItem, FormControl } from '@material-ui/core'
+import { Grid, makeStyles, TextField, Select, InputLabel, MenuItem, FormControl, InputAdornment, Button } from '@material-ui/core'
+import clsx from 'clsx';
 
 const useStyle = makeStyles(theme =>({
     root: {
         '& .MuiFormControl-root':{
             width: '80%',
-            margin:theme.spacing(3)
-        },
-    }
+            margin:theme.spacing(2)
+        }
+    },
+    margin: {
+        margin: theme.spacing(1),
+      },
+    textField: {
+        width: '25ch',
+    },
 }))
 
 const initialValues = {
@@ -17,10 +24,11 @@ const initialValues = {
     class: '',
     builder: '',
     hull: '',
-    lenght: '',
+    length: '',
     category: '',
     model: '',
     fuel: '',
+    weight:''
 }
 
 
@@ -36,8 +44,19 @@ const handleInputChange = e => {
         ...values,
         [name]:value
     })
-    
 }
+function buttonClicked(){
+    console.log(values);
+}
+
+const yearOptions = [];
+const minOffset = 0;
+const maxOffset = 30;
+const thisYear = (new Date()).getFullYear();
+for (let i = minOffset; i <= maxOffset; i++) {
+    const year = thisYear - i;
+    yearOptions.push(<option value={year} key={i}>{year}</option>);
+  }
 
 
     return (
@@ -53,11 +72,13 @@ const handleInputChange = e => {
                     onChange = {handleInputChange}
                     />
                 </Grid>
-                
+            </Grid>
+            <Grid container>
                 <Grid item xs={6}>
                     <TextField
                         multiline
-                        rowsMax={2}
+                        variant="outlined"
+                        rows={6}
                         label="Add Description"
                         name="description"
                         value={values.description}
@@ -68,9 +89,9 @@ const handleInputChange = e => {
             <Grid container>
                 <Grid item xs={6}>
                     <FormControl variant="outlined">
-                        <InputLabel>abc</InputLabel>
+                        <InputLabel>Class</InputLabel>
                         <Select
-                            label="abc"
+                            label="Class"
                             name="class"
                             value={values.class}
                             onChange={handleInputChange}
@@ -162,16 +183,68 @@ const handleInputChange = e => {
                 </Grid>
             </Grid>
             <Grid container>
-                <Grid item xs={2}>
-                    
+                <Grid item xs={6}>
+                    <TextField
+                    variant="outlined"
+                    label="Model"
+                    name="model" 
+                    value={values.model}
+                    onChange = {handleInputChange}
+                    />
                 </Grid>
-                <Grid item xs={2}>
-                    
-                </Grid>
-                <Grid item xs={2}>
-                    
+                
+                <Grid item xs={6}>
+                    <TextField
+                    variant="outlined"
+                    label="Builder"
+                    name="builder" 
+                    value={values.builder}
+                    onChange = {handleInputChange}
+                    />
                 </Grid>
             </Grid>
+            <Grid container>
+                <Grid item xs={2}>
+                    <TextField
+                        label="Length"
+                        id="outlined-start-adornment"
+                        name='length'
+                        onChange={handleInputChange}
+                        className={clsx(classes.margin, classes.textField)}
+                        InputProps={{
+                        endAdornment: <InputAdornment position="end">ft.</InputAdornment>,
+                        }}
+                        variant="outlined"
+                        style={{marginLeft:'30%'}}
+                    />
+                </Grid>
+                <Grid item xs={3}>
+                    <FormControl variant="outlined">
+                        <InputLabel>Make Year</InputLabel>
+                        <Select
+                            label="Make Year"
+                            name="year"
+                            value={values.year}
+                            onChange={handleInputChange}>
+                            <MenuItem value="">
+                                <em>None</em>
+                            </MenuItem>
+                            {/* <MenuItem value={yearOptions}><em>{yearOptions}</em></MenuItem> */}
+                            {yearOptions}
+                            {yearOptions.map((year, index) => <MenuItem key={index} value={year}/>)}
+                        </Select>
+                    </FormControl>
+                </Grid>
+            </Grid>
+            <Button 
+                size="large" 
+                variant="contained" 
+                color="secondary"
+                style={{width:'20%', marginTop:'5%'}}
+                onClick={buttonClicked}
+                >
+                Add
+            </Button>
           </form>
         </div>
     )
