77517f977c06f534ec49933fc3af004a32f5eada
Saksham Jhawar <sakshamjhawar@Sakshams-MacBook-Pro.local>
2021-02-11 15:05:25 -0700

    fixed POST API call

--- tys_webserver/src/main/java/tys/tysWebserver/watercraftManager/controller/AddWatercraftController.java
+++ tys_webserver/src/main/java/tys/tysWebserver/watercraftManager/controller/AddWatercraftController.java
@@ -1,21 +1,25 @@
 package tys.tysWebserver.watercraftManager.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import tys.tysWebserver.watercraftManager.model.AddWatercraftModel;
 import tys.tysWebserver.watercraftManager.repository.AddWatercraftRepo;
+
+@RestController
+@RequestMapping("/watercraft")
 
 public class AddWatercraftController {
 	
 	@Autowired
 	private AddWatercraftRepo AWrepo;
 	
-	@PostMapping("/watercraft")
+	@PostMapping("/details")
 	public AddWatercraftModel createWatercraft(@RequestBody AddWatercraftModel addwatercraft) {
+		System.out.println(addwatercraft.getWatercraftName());
 		return AWrepo.save(addwatercraft);
 		
 	}
