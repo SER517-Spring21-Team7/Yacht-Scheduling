b21034acf0441b8c1dc931e3cc750994b3799264
Abhinaw Sarang <asarang@asu.edu>
2021-02-10 22:19:51 -0700

    Task#43#44 - User profile API implementation

--- tys_webserver/src/main/java/tys/tysWebserver/accountManager/controller/UserAccountController.java
+++ tys_webserver/src/main/java/tys/tysWebserver/accountManager/controller/UserAccountController.java
@@ -1,7 +1,5 @@
 package tys.tysWebserver.accountManager.controller;
 
-import java.util.Optional;
-
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
@@ -12,20 +10,24 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import tys.tysWebserver.accountManager.model.UserNotificationSetting;
+import tys.tysWebserver.accountManager.model.UserProfile;
 import tys.tysWebserver.accountManager.repository.UserNotificationSettingRepo;
+import tys.tysWebserver.accountManager.repository.UserProfileRepo;
 
 @RestController
-public class UserNotificationSettingController {
+public class UserAccountController {
 	
 	@Autowired
 	private UserNotificationSettingRepo UNSRepo;
+	@Autowired
+	private UserProfileRepo UPRepo;
 	
-	
+	// Below methods are for notification setting related API
 	@GetMapping("/user/{id}/nsetting")
 	public ResponseEntity<UserNotificationSetting> getNotificationSettingById(@PathVariable(value = "id") Integer userId)
 			throws ResourceNotFoundException {
 		UserNotificationSetting unsObject = UNSRepo.findById(userId)
-				.orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + userId));
+				.orElseThrow(() -> new ResourceNotFoundException("Notification setting not found for this id :: " + userId));
 		return ResponseEntity.ok().body(unsObject);
 	}
 	
@@ -34,7 +36,7 @@ public class UserNotificationSettingController {
 			@RequestBody UserNotificationSetting unsRequest)
 			throws ResourceNotFoundException {
 		UserNotificationSetting unsObject = UNSRepo.findById(userId)
-				.orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + userId));
+				.orElseThrow(() -> new ResourceNotFoundException("Notification setting not found for this id :: " + userId));
 
 		unsObject.setAddedToExpense(unsRequest.isAddedToExpense());
 		unsObject.setEventCancel(unsRequest.isEventCancel());
@@ -51,4 +53,34 @@ public class UserNotificationSettingController {
 		return ResponseEntity.ok(updatedSetting);
 	}
 
+	// Below methods are for profile related API
+	@GetMapping("/user/{id}/profile")
+	public ResponseEntity<UserProfile> getUserProfileById(@PathVariable(value = "id") Integer userId)
+			throws ResourceNotFoundException {
+		UserProfile upObject = UPRepo.findById(userId)
+				.orElseThrow(() -> new ResourceNotFoundException("Profile not found for id :: " + userId));
+		return ResponseEntity.ok().body(upObject);
+	}
+	
+	@PutMapping("/user/{id}/profile")
+	public ResponseEntity<UserProfile> updateUserProfileById(@PathVariable(value = "id") Integer userId,
+			@RequestBody UserProfile upRequest)
+			throws ResourceNotFoundException {
+		UserProfile upObject = UPRepo.findById(userId)
+				.orElseThrow(() -> new ResourceNotFoundException("Profile not found for id :: " + userId));
+
+		upObject.setFirstName(upRequest.getFirstName());
+		upObject.setLastName(upRequest.getLastName());
+		upObject.setMobile(upRequest.getMobile());
+		upObject.setAlternateMobile(upRequest.getAlternateMobile());
+		upObject.setTimezone(upRequest.getTimezone());
+		upObject.setCountry(upRequest.getCountry());
+		upObject.setAddress_1(upRequest.getAddress_1());
+		upObject.setAddress_2(upRequest.getAddress_2());
+		upObject.setCity(upRequest.getCity());
+		upObject.setState(upRequest.getState());
+		upObject.setZipCode(upRequest.getZipCode());
+		final UserProfile updatedProfile = UPRepo.save(upObject);
+		return ResponseEntity.ok(updatedProfile);
+	}
 }
