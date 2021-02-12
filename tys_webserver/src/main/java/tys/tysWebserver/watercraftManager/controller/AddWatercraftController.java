cde97bb38cec1d7424ffa6d64c2f14c9f1dbe1b8
smit <shahsmit49@gmail.com>
2021-02-12 01:13:34 -0700

    US 22 Edit/delete bug fixed

--- tys_webserver/src/main/java/tys/tysWebserver/watercraftManager/controller/AddWatercraftController.java
+++ tys_webserver/src/main/java/tys/tysWebserver/watercraftManager/controller/AddWatercraftController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,7 +40,7 @@ public class AddWatercraftController {
 	}
 	
 	@CrossOrigin(origins = "http://localhost:3000")
-	@GetMapping("/deleteWaterCraft/{id}")
+	@DeleteMapping("/deleteWaterCraft/{id}")
 	public ResponseEntity<String> deleteWaterCraft(@PathVariable String id) {
 		System.out.println(id);
 		AWrepo.deleteById(Integer.parseInt(id));
