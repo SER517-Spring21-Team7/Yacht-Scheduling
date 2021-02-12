cd75079c307de449d8e801ceb3da29ffd5c7a13a
smit <shahsmit49@gmail.com>
2021-02-11 20:06:15 -0700

    US21 CORS issue resolved

--- tys_webserver/src/main/java/tys/tysWebserver/watercraftManager/controller/AddWatercraftController.java
+++ tys_webserver/src/main/java/tys/tysWebserver/watercraftManager/controller/AddWatercraftController.java
@@ -3,6 +3,7 @@ package tys.tysWebserver.watercraftManager.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,6 +28,7 @@ public class AddWatercraftController {
 		
 	}
 	
+	@CrossOrigin(origins = "http://localhost:3000")
 	@GetMapping("/getAllWaterCraft")
 	public List<WatercraftModel> finalAll() {
 		List<WatercraftModel> data = AWrepo.findAll();
