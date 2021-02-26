cb3fdb7dea74167772ef3a011d9138b5233f9841
smit <shahsmit49@gmail.com>
2021-02-25 21:46:19 -0700

    UI changes

--- tys_webclient/src/listMember/ListMember.js
+++ tys_webclient/src/listMember/ListMember.js
@@ -1,12 +1,30 @@
 import React from 'react'
 import Member from './Member'
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
 
 const ListMember = () => {
     return (
-        <div>
-            <Member></Member>
-            <Member></Member>
-            <Member></Member>
+        <div className={useStyles.root}>
+            <Grid container direction="row" alignItems="center">
+                <Member></Member>
+                <Member></Member>
+                <Member></Member>
+                <Member></Member>
+                <Member></Member>
+                <Member></Member>
+            </Grid>
         </div>
     )
 }
--- tys_webclient/src/listMember/Member.js
+++ tys_webclient/src/listMember/Member.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
+import Grid from '@material-ui/core/Grid';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles({
-  root: {
+    root: {
+        display: 'flex',
         width: 345,
-      height:300
+        height:600
   },
+  media: {  
+        height: 50,
+        width: 50,
+    },
   
 });
 
@@ -20,25 +23,76 @@ const Member = () => {
 
     const classes = useStyles();
     const bull = <span className={classes.bullet}>•</span>;
-    
+    var pix = '7px';
+    const section = {
+        marginTop: pix,
+        marginLeft: pix,
+        marginRight: pix,
+        marginBotton: pix,
+    };
     return (
         <>
-            <Card className={classes.root}>
-                <div style={{ flexDirection: 'column' }}>
-                    <div style={{display: 'flex', flexDirection: 'row', "height":"30%"}}>
-                        <div style={{display: 'flex', "height":"50%"}}>hiiiiiiiiiiiiii1</div>
-                        <div style={{display: 'flex', "height":"50%"}}>hiiiiiiiiiiiii2</div>
-                    </div>
-                    <div style={{display: 'flex', flexDirection: 'column', "height":"60%"}}>
-                        byeeeeeeeeeeeee
-                         byeeeeeeeeeeeee
-                    </div>
-                    <div style={{display: 'flex', flexDirection: 'row', "height":"10%"}}>
-                        <div>bye11111</div>
-                        <div>bye11111111</div>
-                    </div>
-                </div>
-        </Card>
+            <Card className={classes.root} style={ section}>
+                <Grid container direction="row" style={ section}>
+                    <Grid container spacing={2} style={{ height: "25%" }}  >
+                        <Grid item xs={ 4} spacing={2}>
+                            <CardMedia
+                                className={classes.media}
+                                image="https://api1.nauticalmonkey.com/uploadedImages/ab68874bf7d941cb815e5ba28d0a7b07_small.jpg"
+                                title="Boat"
+                                alignItem='center'
+                            />
+                        </Grid>
+                        <Grid item xs={ 8} spacing={2}>
+                           <Typography gutterBottom variant="h5" component="h2">
+                                Namee
+                            </Typography> 
+                            <Typography variant="h6" gutterBottom>
+                                Premium & Standard
+                            </Typography> 
+                        </Grid>
+                    </Grid>
+                    
+                    <Grid container spacing={2} style={{ height: "65%" }} >
+                        <Grid item xs={12}>
+                            <Typography variant="body1" gutterBottom style={{ whiteSpace: 'pre-line' }}>
+                                email:sshah73@asu.edu
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Typography variant="body1" gutterBottom style={{ whiteSpace: 'pre-line' }}>
+                                Access dates: 10/17
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Typography variant="body1" gutterBottom style={{ whiteSpace: 'pre-line' }}>
+                                Premium slot
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Typography variant="body1" gutterBottom style={{ whiteSpace: 'pre-line' }}>
+                                Standard slot
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Typography variant="body1" gutterBottom style={{ whiteSpace: 'pre-line' }}>
+                                Limited to 0 FREE bookings per month.
+                            </Typography>
+                        </Grid>
+                        
+                    </Grid>
+
+                    <Grid container spacing={2}  style={{ height: "15%" }}>
+                        <Button size="small" color="primary" >
+                            Edit
+                        </Button>
+                        <Button size="small" color="primary" >
+                            Delete Member
+                        </Button>
+                    </Grid>
+                </Grid>
+                
+            </Card>
         </>
     )
 }
