d2e380a6d7a6b1bac0a16e554fd31338d3f968d0
Abhinaw Sarang <asarang@asu.edu>
2021-04-08 13:13:01 -0700

    Implemeted get slots for user API

--- tys_webserver/src/main/java/tys/tysWebserver/memberManager/controller/MemberSlotController.java
+++ tys_webserver/src/main/java/tys/tysWebserver/memberManager/controller/MemberSlotController.java
@@ -1,7 +1,12 @@
 package tys.tysWebserver.memberManager.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import tys.tysWebserver.memberManager.model.MemberSlot;
@@ -17,6 +22,13 @@ public class MemberSlotController {
 	
 	@Autowired
 	SchedulerSettingController ssController;
+	
+	@GetMapping("/getslots/{mid}")
+	public ResponseEntity<MemberSlot> getSlotsByUser(@PathVariable(value="mid") Integer mid)
+		throws ResourceNotFoundException {
+		MemberSlot memberSlot = msr.findByMemberId(mid).get(0);
+			return ResponseEntity.ok().body(memberSlot);
+	}
 
 	public void createMemberSlot(int memberId, int watercraftId, int slotCount) {
 		int holidaySlotCount = ssController.findMaxHolidayBookingDaysByWatercraftId(watercraftId);
--- tys_webserver/src/main/java/tys/tysWebserver/memberManager/repository/MemberSlotRepository.java
+++ tys_webserver/src/main/java/tys/tysWebserver/memberManager/repository/MemberSlotRepository.java
@@ -1,6 +1,8 @@
 package tys.tysWebserver.memberManager.repository;
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,7 @@ import tys.tysWebserver.memberManager.model.MemberSlot;
 
 @Repository
 public interface MemberSlotRepository extends JpaRepository<MemberSlot, Integer> {
+	
+	List<MemberSlot> findByMemberId(Integer id);
 
 }
